4ac47c47fcab2ecc4201e99c2bf722ce2d56f531
smalyala <sahith.malyala325@gmail.com>
2014-07-10 16:00:52 -0400

    extra

--- Handlebars.js
+++ Handlebars.js
@@ -5,14 +5,6 @@ if (Meteor.isClient) {
   Template.list.people = function () {
 	return Peeps.find({});
   };
-
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
 }
 
 if (Meteor.isServer) {
