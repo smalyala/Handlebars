ab876a3e18e32c2840fbf709fcd283ddf7c6638f
smalyala <sahith.malyala325@gmail.com>
2014-07-10 15:48:13 -0400

    init

---
+++ Handlebars.js
@@ -0,0 +1,32 @@
+
+
+Peeps = Meteor.collection("people");
+
+if (Meteor.isClient) {
+  Template.list.people
+  };
+
+  Template.hello.events({
+    'click input': function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+		if (Peeps.find().count() == 0){
+			var names = ["Ada Lovelace",
+						"Grace Hopper",
+					    "Marie Curie",
+					    "Carl Friedrich Gauss",
+					    "Nikola Tesla",
+					    "Claude Shannon"];
+		}
+		for (var i = 0; i < names.length; i++){
+			Peeps.insert({name: names[i]});
+		}
+  });
+}
